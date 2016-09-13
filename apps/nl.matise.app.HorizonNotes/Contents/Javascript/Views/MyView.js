784829fee4ea8568d0d7272f31bb287f9fa91b02
Tim Borst <tim@matise.nl>
2016-09-13 18:59:34 +0200

    Made epic js things

--- apps/nl.matise.app.HorizonNotes/Contents/Javascript/Views/MyView.js
+++ apps/nl.matise.app.HorizonNotes/Contents/Javascript/Views/MyView.js
@@ -1,120 +1,34 @@
 // Create a class and extended it from the MAF.system.SidebarView
 var MyView = new MAF.Class({
 	ClassName: 'MyView',
+
 	Extends: MAF.system.FullscreenView,
 
-	initialize: function() {
-		this.parent();
-
-		// Create a Room across all households
-		// this.room = new MAF.Room( this.ClassName );
-		// Create a Room for this specific household
-		this.room = new MAF.PrivateRoom('MatiseFissa');
-		console.log(this.room);
-	},
-
-
 	// Create your view template
-	createView: function() {
-		var room = this.room;
-		// Keep track of the clients connected
-		var clients = {};
+	createView: function () {
 		// Reference to the current view
 		var view = this;
-		view.elements.topContainer = new MAF.element.Container({
-			styles: {
-				width: 200,
-				height: 200,
+		view.elements.topContainer = new MAF.element.Container( {
+	    styles:{
+	      width: 840,
+	      height: 1080,
 				hAlign: 'right',
-				vOffset: 100,
-				hOffset: 100,
-			},
+				vOffset: 0,
+				hOffset: 120,
+	    },
 			ClassName: 'topContainer'
-		}).appendTo(this);
-		view.elements.topContainer.element.innerHTML = '<p>Hoi wereld</p>';
-
-		// Set listeners for Room and Connection
-		(function(evt) {
-			var payload = evt.payload;
-
-			switch (evt.type) {
-				case 'onConnected':
-					log('room connected');
-					// If connected but room not joined make sure to join it automaticly
-					if (!room.joined) room.join();
-					return;
-				case 'onDisconnected':
-					clients = {}; // Reset clients
-					log('connection lost waiting for reconnect and automaticly rejoin');
-					return;
-				case 'onCreated':
-					// Create an url to the client application and pass the hash as querystring
-					log('room created', payload.hash);
-					return;
-				case 'onDestroyed':
-					clients = {}; // Reset clients
-					log('room destroyed', payload.hash);
-					return;
-				case 'onJoined':
-					// If user is not the app then log the user
-					if (payload.user !== room.user)
-						log('user joined', payload.user);
-					return;
-				case 'onHasLeft':
-					// If user is not the app then log the user
-					if (payload.user !== room.user)
-						log('user has left', payload.user);
-					return;
-				case 'onData':
-					var data = payload.data;
-
-					if ('draw' === data.e)
-						return draw(data.c, data.k, data.x, data.y);
-					if ('clear' === data.e)
-						return reset();
-					break;
-				default:
-					log(evt.type, payload);
-					break;
-			}
-		}).subscribeTo(
-			room, [
-				'onConnected',
-				'onDisconnected',
-				'onCreated',
-				'onDestroyed',
-				'onJoined',
-				'onHasLeft',
-				'onData',
-				'onError'
-			]
-		);
-
-		// If Room socket is connected create and join room
-		if (room.connected) room.join();
-
+	  } ).appendTo( this );
+		view.elements.topContainer.element.innerHTML = '<div class="sideScreen"><div class="left"><div class="row openApp"><div class="black"><p class="text">Quick open your App!</p><span><span></div></div><div class="row notification"><div class="black"><div class="profile"></div><p class="text">120 total</p></div><div class="white"><p class="text">+ 50 points</p><span><span></div></div><div class="row notification"><div class="black"><div class="profile"></div><p class="text">90 total</p></div><div class="white"><p class="text">+ 0 points</p><span><span></div></div></div><div class="right"><div class="iconHolder"><div class="icon"><div class="actualIcon"></div></div><div class="loader loader--style2" title="1"><svg version="1.1" id="loader-1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="160px" height="160px" viewBox="0 0 50 50" xml:space="preserve"><path fill="#000" d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"><animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="1s" repeatCount="indefinite"/></path></svg></div></div></div></div>';
 	},
 
 	// After create view and when returning to the view
 	// the update view is called
-	updateView: function() {
+	updateView: function () {
 		// Reference to the current view
 		var view = this;
 		this.helloWorld();
 	},
-	helloWorld: function() {
+	helloWorld: function () {
 		console.log('hello world');
-	},
-	destroyView: function() {
-		if (this.room) {
-			// Leave room, will trigger an onLeaved of the app user
-			this.room.leave();
-
-			// Destroy the room
-			this.room.destroy();
-
-			// Unreference from view for GC
-			delete this.room;
-		}
 	}
 });
--- apps/nl.matise.app.HorizonNotes/Contents/Styles/theme.js
+++ apps/nl.matise.app.HorizonNotes/Contents/Styles/theme.js
@@ -1,3 +1,13 @@
-Theme.set(
-  {"topContainer": {"styles": {"backgroundColor": "#2ddfbb"} } } 
-);
+Theme.set({
+  "left":             {"styles": {"backgroundColor": "#FF00FF", "width": "708px", "top": "74px", "height": "100%", "position": "absolute", "left": "0px" } },
+  "right":            {"styles": {"width": "132px", "height": "100%", "position": "absolute", "right": "0px" } },
+  "iconHolder":       {"styles": {"overflow": "hidden", "width": "132px", "height": "132px", "top": "64px", "position": "absolute" } },
+  "icon":             {"styles": {"backgroundColor": "#2DDFBB", "overflow": "hidden", "width": "100px", "height": "100px", "top": "16px", "left": "16px", "zIndex": "2", "borderRadius": "50%", "position": "absolute" } },
+  "actualIcon":       {"styles": {"backgroundImage": "url(/apps/nl.matise.app.HorizonNotes/Contents/Images/icon.svg)", "width": "100px", "height": "100px" } },
+  "loader":           {"styles": {"overflow": "hidden", "width": "160px", "height": "160px", "top": "-14px", "left": "-14px", "zIndex": "1", "borderRadius": "50%", "position": "absolute" } },
+  "row":              {"styles": {"height": "80px", "width": "100%", "backgroundColor": "white", "margin": "10px 0px", "textAlign": "right"} },
+  "black":            {"styles": {"height": "80px","display": "inline-block", "borderRadius": "40px", "width": "auto", "padding": "0px 40px", "lineHeight": "80px", "backgroundColor": "black", "textAlign": "right"} },
+  "white":            {"styles": {"height": "80px","display": "inline-block", "borderRadius": "40px", "width": "auto", "padding": "0px 40px", "lineHeight": "80px", "backgroundColor": "black", "textAlign": "right"} },
+  "topContainer":     {"styles": {"backgroundColor": "#CCC"} },
+  "text":             {"styles": {"fontSize": "15px", "display": "block", "height": "80px"} }
+});
